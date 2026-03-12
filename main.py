2293887d762535d3da8bc0f9637c7400af16dd56
ram <ilyataran13@gmail.com>
2026-03-12 17:47:18 +0000

    code to store a list of messages in the conversation and pass in the "role" appropriately.

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 parser = argparse.ArgumentParser(description="Chatbot")
 
 parser.add_argument("user_prompt", type=str, help="User prompt")
 
 args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,7 +23,7 @@ client = genai.Client(api_key=api_key)
 
 def main():
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents= args.user_prompt)
+    model='gemini-2.5-flash', contents= messages)
     print("Response:")
     print(response.text)
     if not response.usage_metadata:
