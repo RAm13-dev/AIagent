24db1770bc78b12c85c4400f00f0b7238f861161
ram <ilyataran13@gmail.com>
2026-03-12 17:39:04 +0000

    accept the prompt as a command-line argument

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
 
+parser = argparse.ArgumentParser(description="Chatbot")
+
+parser.add_argument("user_prompt", type=str, help="User prompt")
+
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,8 +19,7 @@ client = genai.Client(api_key=api_key)
 
 def main():
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-    )
+    model='gemini-2.5-flash', contents= args.user_prompt)
     print("Response:")
     print(response.text)
     if not response.usage_metadata:
